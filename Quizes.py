6c05731ced80fa0c8d46e098f0c290ab6d705e9e
Bruce Nielson <brucenielson2@gmail.com>
2017-04-09 10:31:52 -0600

    Copied all quiz code so I can refer to it later

--- Quizes.py
+++ Quizes.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     test_run()
 
 
-# 02-02 Quiz Utility Functions for Reading Data
+# 01-02 Quiz Utility Functions for Reading Data
 """Utility functions"""
 
 import os
@@ -105,7 +105,7 @@ if __name__ == "__main__":
 
 
 
-# 02-02 Slice and plot two stocks
+# 01-02 Slice and plot two stocks
 """Slice and plot"""
 
 import os
@@ -169,6 +169,190 @@ if __name__ == "__main__":
 
 
 
+# 01-03 Quiz Locate Maximum Value
+"""Locate maximum value."""
+import numpy as np
+
+def get_max_index(a):
+    """Return the index of the maximum value in given 1D array."""
+    return np.argmax(a)
+
+
+def test_run():
+    a = np.array([9, 6, 2, 3, 12, 14, 7, 10], dtype=np.int32)  # 32-bit integer array
+    print
+    "Array:", a
+
+    # Find the maximum and its index in array
+    print
+    "Maximum value:", a.max()
+    print
+    "Index of max.:", get_max_index(a)
+
+
+if __name__ == "__main__":
+    test_run()
+
+# 01-04 Quiz Calculate Bollinger Bands
+"""Bollinger Bands."""
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def symbol_to_path(symbol, base_dir="data"):
+    """Return CSV file path given ticker symbol."""
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, 'SPY')
+
+    for symbol in symbols:
+        df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date',
+                              parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
+        df_temp = df_temp.rename(columns={'Adj Close': symbol})
+        df = df.join(df_temp)
+        if symbol == 'SPY':  # drop dates SPY did not trade
+            df = df.dropna(subset=["SPY"])
+
+    return df
+
+
+def plot_data(df, title="Stock prices"):
+    """Plot stock prices with a custom title and meaningful axis labels."""
+    ax = df.plot(title=title, fontsize=12)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price")
+    plt.show()
+
+
+def get_rolling_mean(values, window):
+    """Return rolling mean of given values, using specified window size."""
+    return pd.rolling_mean(values, window=window)
+
+
+def get_rolling_std(values, window):
+    """Return rolling standard deviation of given values, using specified window size."""
+    # TODO: Compute and return rolling standard deviation
+    return pd.rolling_std(values, window=window)
+
+
+def get_bollinger_bands(rm, rstd):
+    """Return upper and lower Bollinger Bands."""
+    # TODO: Compute upper_band and lower_band
+    upper_band = rm + rstd * 2
+    lower_band = rm - rstd * 2
+    return upper_band, lower_band
+
+
+def test_run():
+    # Read data
+    dates = pd.date_range('2012-01-01', '2012-12-31')
+    symbols = ['SPY']
+    df = get_data(symbols, dates)
+
+    # Compute Bollinger Bands
+    # 1. Compute rolling mean
+    rm_SPY = get_rolling_mean(df['SPY'], window=20)
+
+    # 2. Compute rolling standard deviation
+    rstd_SPY = get_rolling_std(df['SPY'], window=20)
+
+    # 3. Compute upper and lower bands
+    upper_band, lower_band = get_bollinger_bands(rm_SPY, rstd_SPY)
+
+    # Plot raw SPY values, rolling mean and Bollinger Bands
+    ax = df['SPY'].plot(title="Bollinger Bands", label='SPY')
+    rm_SPY.plot(label='Rolling mean', ax=ax)
+    upper_band.plot(label='upper band', ax=ax)
+    lower_band.plot(label='lower band', ax=ax)
+
+    # Add axis labels and legend
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price")
+    ax.legend(loc='upper left')
+    plt.show()
+
+
+if __name__ == "__main__":
+    test_run()
+
+
+# 01-04 Quiz Calculate Daily Returns
+"""Compute daily returns."""
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def symbol_to_path(symbol, base_dir="data"):
+    """Return CSV file path given ticker symbol."""
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, 'SPY')
+
+    for symbol in symbols:
+        df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date',
+                              parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
+        df_temp = df_temp.rename(columns={'Adj Close': symbol})
+        df = df.join(df_temp)
+        if symbol == 'SPY':  # drop dates SPY did not trade
+            df = df.dropna(subset=["SPY"])
+
+    return df
+
+
+def plot_data(df, title="Stock prices", xlabel="Date", ylabel="Price"):
+    """Plot stock prices with a custom title and meaningful axis labels."""
+    ax = df.plot(title=title, fontsize=12)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    plt.show()
+
+
+def compute_daily_returns(df):
+    """Compute and return the daily return values."""
+    # TODO: Your code here
+    # Note: Returned DataFrame must have the same number of rows
+    # pd.options.display.float_format = '{:.10f}'.format
+    dr = df.copy()
+    dr[1:] = (df[1:] / df[:-1].values) - 1
+    dr.ix[0, :] = 0
+    print
+    dr.head()
+    print
+    dr.tail()
+    return dr
+
+
+def test_run():
+    # Read data
+    dates = pd.date_range('2012-07-01', '2012-07-31')  # one month only
+    symbols = ['SPY', 'XOM']
+    df = get_data(symbols, dates)
+    plot_data(df)
+
+    # Compute daily returns
+    daily_returns = compute_daily_returns(df)
+    plot_data(daily_returns, title="Daily returns", ylabel="Daily returns")
+
+
+if __name__ == "__main__":
+    test_run()
+
+
+#01-05 Quiz Fill Missing Values
 """Fill missing values"""
 
 import numpy as np
