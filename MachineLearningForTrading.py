ec6517268540812eb6a695f03cdd31055b2b3155
Bruce Nielson <brucenielson2@gmail.com>
2017-04-09 15:01:57 -0600

    Modularizing

--- MachineLearningForTrading.py
+++ MachineLearningForTrading.py
@@ -5,6 +5,36 @@ from importlib import reload
 import matplotlib.pyplot as plt
 
 
+def get_data(syms, sd, ed):
+    # TODO: In the real class I'll load CSV files and I am not sure adding SPY then dropping it will work with the CSV files or not
+    dates = pd.date_range(sd, ed)
+    df = pd.DataFrame(index=dates)
+
+    # I always insert SPY at the front even if it's already in the list. This way I can drop it freely afterwards but still be sure
+    # all trading days are accounted for
+    syms.insert(0, 'SPY')
+
+    do_once = True
+    for symbol in syms:
+        data = pdr.DataReader(symbol, 'yahoo', sd, ed)
+        df = df.join(data['Adj Close'])# , how='inner')
+        df = df.rename(columns={'Adj Close': symbol})
+        if symbol == 'SPY' and do_once:  # drop dates SPY did not trade
+            df = df.dropna(subset=["SPY"])
+            do_once = False
+            df = df.rename(columns={'SPY':'DROP'})
+
+    # Fill in missing values
+    df.fillna(method='ffill', inplace=True)
+    df.fillna(method='bfill', inplace=True)
+
+    # Drop the SPY I added
+    df = df.drop('DROP', axis=1)
+
+    return df
+
+
+
 def assess_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,1,1), syms=['GOOG','AAPL','GLD','XOM'], allocs=[0.1,0.2,0.3,0.4],
     sv=1000000, rfr=0.0, sf=252.0, gen_plot=False):
     """
@@ -25,35 +55,28 @@ def assess_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,1,1), syms=['
     sf: Sampling frequency per year
     gen_plot: If False, do not create any output. If True it is OK to output a plot such as plot.png
     """
-    dates = pd.date_range(sd, ed)
-    df = pd.DataFrame(index=dates)
+    # TODO: I don't know how to use sampling frequency. Seems useless to me because you are being given one input per day no matter what your date range is
+    df = get_data(syms, sd, ed)
 
-    if 'SPY' not in syms:  # add SPY for reference, if absent
-        syms.insert(0, 'SPY')
-
-    for symbol in syms:
-        data = pdr.DataReader(symbol, 'yahoo', sd, ed)
-        df = df.join(data['Adj Close'])# , how='inner')
-        df = df.rename(columns={'Adj Close': symbol})
-        if symbol == 'SPY':  # drop dates SPY did not trade
-            df = df.dropna(subset=["SPY"])
-
-    # Fill in missing values
-    df.fillna(method='ffill', inplace=True)
-    df.fillna(method='bfill', inplace=True)
-    # Show plot
-    plot_data(df)
-
-    df_temp = df / df.ix[0]
-    plot_data(df_temp)
+    # Normalize data
+    df_norm = df / df.ix[0]
 
     # Gather Stats
-    cr = df_temp.ix[-1] - df_temp.ix[0]
-    print(cr)
+    cr = df_norm.ix[-1] - df_norm.ix[0]
+    #print(cr)
+
+    # Create Plots
+    if gen_plot:
+        # Show plot
+        plot_data(df)
+        plot_data(df_norm, xlabel="Stock Returns", ylabel='Return')
+
+    return cr
     #return cr, adr, sddr, ev
 
 
-def plot_data(df, title="Stock prices", xlabel="Date", ylabel="Price"):
+
+def plot_data(df, title="Stock Prices", xlabel="Date", ylabel="Price"):
     """Plot stock prices with a custom title and meaningful axis labels."""
     ax = df.plot(title=title, fontsize=12)
     ax.set_xlabel(xlabel)
@@ -92,5 +115,6 @@ cr, adr, sddr, sr, ev = assess_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetim
 plt.ion()
 
 #cr, adr, sddr, sr, ev =
-assess_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,1,1), syms=['GOOG','AAPL','GLD','XOM'],
-    allocs=[0.1,0.2,0.3,0.4], sv=1000000, rfr=0.0, sf=252.0, gen_plot=False)
+cr = assess_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,1,1), syms=['GOOG','AAPL','GLD','XOM'],
+    allocs=[0.1,0.2,0.3,0.4], sv=1000000, rfr=0.0, sf=252.0, gen_plot=True)
+
