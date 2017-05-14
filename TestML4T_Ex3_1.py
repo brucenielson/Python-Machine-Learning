df230d4b4d4b14815ebff4f8115e4ec1ec5ef2c4
Bruce Nielson <brucenielson2@gmail.com>
2017-05-14 01:10:15 -0600

    Fixed tests

--- TestML4T_Ex3_1.py
+++ TestML4T_Ex3_1.py
@@ -7,14 +7,13 @@ if __name__ == '__main__':
     unittest.main()
 
 
-class TestCalc_entropy(unittest.TestCase):
+class TestML4T_Ex3_1(unittest.TestCase):
     def test_calc_entropy(self):
         a = pd.DataFrame([[0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
         result = ML4T_Ex3_1.calc_entropy(a)
-        self.assertEqual(result, 0.94028595867063092)
+        self.assertAlmostEqual(result, 0.94028595867063092)
 
-#    def calc_feature_entropy(self):
-        # TODO: Fix tests -- why do I need to handle setup
+    def test_calc_feature_entropy(self):
         a = pd.DataFrame([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [0, 1], [0, 1], [0, 1], [1, 0], [1, 0], [0, 0], [0, 0], [0, 0]], columns=['feature', 'result'])
         result = ML4T_Ex3_1.calc_feature_entropy(a, 'feature')
         print(result)
